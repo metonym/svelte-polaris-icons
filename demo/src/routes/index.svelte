9e55204c3f76d145858b80daf7f0eab68c7288f4
Eric Liu <ericyl.us@gmail.com>
2020-11-29 16:46:58 -0800

    Upgrade svelte-polaris-icons (#1)
    * chore(deps-dev): upgrade svg-to-svelte
    
    * feat: upgrade @shopify/polaris-icons to version 4.1.0
    
    * chore: use svelte-readme for documentation

--- demo/src/routes/index.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Component from "svelte-polaris-icons";
-</script>
-
-<svelte:head>
-  <title>svelte-polaris-icons</title>
-</svelte:head>
-
-<Component />
---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import svelteReadme from "svelte-readme";
+
+export default svelteReadme({
+  minify: !process.env.ROLLUP_WATCH,
+  svelte: { immutable: true },
+  prefixUrl: "https://github.com/metonym/svelte-polaris-icons/tree/master/",
+  style: `
+    .code-fence > div {
+      display: inline-flex;
+      flex-direction: column;
+      border: 1px solid #eaecef;
+      border-radius: 4px;
+      padding: 8px;
+      margin: 4px;
+    }
+
+    .code-fence svg {
+      margin-bottom: 4px;
+    }
+  `,
+});
